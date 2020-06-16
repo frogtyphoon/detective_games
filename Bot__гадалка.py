b4b4092e06a336ed98fe4106afc90ee36913a10a
Alexey Ostrovsky <lesha_ostrovskiy@mail.ru>
2020-06-16 21:09:19 +0700

    пример функции

--- Bot__гадалка.py
+++ Bot__гадалка.py
@@ -1,1 +1,24 @@
-print('привет!')
+'''
+def bot_witch(*kwargs):
+    elems = kwargs
+    print(elems[0][0][0], elems[0][0][1])
+
+
+bot_witch(
+    [['text on btn', 'answer'], ['text on btn 1', 'answer 1'], ['text on btn', 'answer'], ['text on btn', 'answer'],
+     ['text on btn', 'answer']])
+
+
+def main():
+    if True:
+        bot_witch([['паша лох', 'да, конечно!'],
+                   ['нет ты лох', 'а в бубен?'],
+                   ['', ''],
+                   ['', ''],
+                   ['', ''],
+                   ['', ''],
+                   ['', '']
+                   ])
+    else:
+        bot_witch()
+'''
