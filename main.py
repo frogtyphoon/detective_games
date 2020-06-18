9d1c297d389d0fb1b3de50f67a309b6407a1618c
Roman <66955831+chibiherbie@users.noreply.github.com>
2020-06-18 14:14:06 +0700

    какая-то херня, HELP!

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-import Bot__admin
+import telebot
+import config
+from telebot import types
+import time
+
+# import Bot__admin
+# import Bot__help
+from Bot__police import bot_police_main
+# import Bot__гадалка
+# import Bot__журналист
+# import Bot__криминалист
+
+relationships_Bot__police, relationships_Bot__admin = 0, 0
+
 
 #  отправка первого сообщения без ввода
-if __name__ == '__main__':
-    Bot__admin
+# bot = telebot.TeleBot(config.TOKEN_POLICE)
+# bot.send_message('991296393', 'Чёрт, Паша, ты где пропал?', parse_mode='html')
+# bot.send_message('991296393', 'Уже неделя прошла... Где ты пропадаешь???', parse_mode='html')
+# bot.send_message('991296393', 'Если не отвечаешь через неделю, то ты уволен!', parse_mode='html')
+# bot.send_message('991296393', 'У тебя осталось 24 часа и ты не будешь стоять рядом со мной!', parse_mode='html')
+
+check = bot_police_main(0)
+if bool(check):
+    print(0)
+
